b9e91da4e9bef0e7f1378da06103fc07e4d19ede
Brandon <brando512@gmail.com>
2021-03-25 16:24:04 -0700

    add new 6kyu find the pairity outlier

---
+++ 6kyu/findThePairityOutlier.py
@@ -0,0 +1,22 @@
+def find_outlier(integers):
+    countE = 0
+    countO = 0
+    
+    
+    for int in integers:
+        if int%2 == 0:
+            countE += 1
+        elif int%2 == 1:
+            countO += 1
+        
+        print(countO)
+        
+        if countE > 1:
+            for num in integers:
+                if num % 2 == 1:
+                    return num
+        
+        elif countO > 1:
+            for num in integers:
+                if num % 2 == 0:
+                    return num
